8c3fef8b36199019c1b2d284eddcd52d591352df
shifat97 <shifatbinreza2@gmail.com>
2026-06-16 19:40:58 +0600

    add: tests with different input validation for home search field

--- test/pages/DestinationPage.ts
+++ test/pages/DestinationPage.ts
@@ -3,10 +3,12 @@ import { Page, Locator, expect } from '@playwright/test';
 export class DestinationPage {
     readonly page: Page;
     readonly destinationGridContainer: Locator;
+    readonly noDestinationFoundText: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.destinationGridContainer = page.locator("//div[@class='destination-grid']");
+        this.noDestinationFoundText = page.locator('//div[@class="no-results"]//h3');
     }
 
     async navigate() {
@@ -20,6 +22,11 @@ export class DestinationPage {
 
     async getAllDestinationGrids(): Promise<Locator[]> {
         return await this.destinationGridContainer.all();
+    }
+
+    async getNoDestinationFoundText(): Promise<string> {
+        await expect(this.noDestinationFoundText).toBeVisible();
+        return (await this.noDestinationFoundText.textContent()) ?? '';
     }
 
     // async search(locationName: string) {
--- test/tests/home.spec.ts
+++ test/tests/home.spec.ts
@@ -22,10 +22,117 @@ test.describe('Home page search workflow', () => {
             await expect(grid).toBeVisible();
         }
     });
+
     /* Test with existed country name */
+    test('search existed country name → result found', async ({ page, homePage, destinationPage }) => {
+        await homePage.search(testData.searchLocations.validCountryName);
+        await expect(page).toHaveURL(
+            `${process.env.BASE_URL}/destinations?search=${testData.searchLocations.validCountryName}`,
+        );
+
+        const count = await destinationPage.destinationGridCounter();
+        expect(count).toBeGreaterThanOrEqual(1);
+
+        const grids = await destinationPage.getAllDestinationGrids();
+
+        for (const grid of grids) {
+            await expect(grid).toBeVisible();
+        }
+    });
+
     /* Test location that doesn't exists */
+    test('search location that does not exist → result not found found', async ({
+        page,
+        homePage,
+        destinationPage,
+    }) => {
+        await homePage.search(testData.searchLocations.invalidLocationName);
+        await expect(page).toHaveURL(
+            `${process.env.BASE_URL}/destinations?search=${testData.searchLocations.invalidLocationName}`,
+        );
+
+        const getText = await destinationPage.getNoDestinationFoundText();
+
+        expect(getText).toEqual('No destinations found');
+    });
+
     /* Test location with blank filed */
+    test('search with blank field → field is required', async ({ homePage }) => {
+        await expect(homePage.searchInput).toHaveAttribute('required');
+    });
+
     /* Test search location with trailing spaces */
+    test('search location with trailing spaces → must trim the spaces and result found', async ({
+        page,
+        homePage,
+        destinationPage,
+    }) => {
+        await homePage.search(`   ${testData.searchLocations.validLocationName}   `);
+        await expect(page).toHaveURL(
+            `${process.env.BASE_URL}/destinations?search=${testData.searchLocations.validLocationName}`,
+        );
+
+        const count = await destinationPage.destinationGridCounter();
+        expect(count).toBeGreaterThanOrEqual(1);
+
+        const grids = await destinationPage.getAllDestinationGrids();
+
+        for (const grid of grids) {
+            await expect(grid).toBeVisible();
+        }
+    });
+
     /* Test search location with different character cases */
+    test('search location with different character combinations → result found', async ({
+        page,
+        homePage,
+        destinationPage,
+    }) => {
+        const combinationList = [
+            testData.searchLocations.validLocationName.toUpperCase(),
+            testData.searchLocations.validLocationName.toLowerCase(),
+            testData.searchLocations.validLocationName.charAt(0).toUpperCase() +
+                testData.searchLocations.validLocationName.slice(1),
+        ];
+
+        for (const combination of combinationList) {
+            await homePage.search(combination);
+            await expect(page).toHaveURL(`${process.env.BASE_URL}/destinations?search=${combination}`);
+
+            const count = await destinationPage.destinationGridCounter();
+            expect(count).toBeGreaterThanOrEqual(1);
+
+            const grids = await destinationPage.getAllDestinationGrids();
+
+            for (const grid of grids) {
+                await expect(grid).toBeVisible();
+            }
+
+            await homePage.navigate();
+        }
+    });
+
     /* Test search location on enter key press */
+    test('search existed location + enter key press → result found @debug', async ({
+        page,
+        homePage,
+        destinationPage,
+    }) => {
+        await homePage.searchInput.fill(testData.searchLocations.validLocationName);
+
+        await page.keyboard.press('Enter');
+
+        await expect(page).toHaveURL(
+            `${process.env.BASE_URL}/destinations?search=${testData.searchLocations.validLocationName}`,
+        );
+
+        const count = await destinationPage.destinationGridCounter();
+        expect(count).toBeGreaterThanOrEqual(1);
+
+        const grids = await destinationPage.getAllDestinationGrids();
+
+        for (const grid of grids) {
+            await expect(grid).toBeVisible();
+        }
+    });
 });
