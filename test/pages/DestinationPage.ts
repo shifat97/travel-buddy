0222b917c208b7969ddfb56896f27f7983a28357
shifat97 <shifatbinreza2@gmail.com>
2026-06-17 15:29:59 +0600

    feat: add search and category filter page objects and implement corresponding destination tests

--- test/pages/DestinationPage.ts
+++ test/pages/DestinationPage.ts
@@ -4,11 +4,19 @@ export class DestinationPage {
     readonly page: Page;
     readonly destinationGridContainer: Locator;
     readonly noDestinationFoundText: Locator;
+    readonly searchInput: Locator;
+    readonly categoryFilters: Locator;
+    readonly categoryFiltersActive: Locator;
+    readonly cardBadge: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.destinationGridContainer = page.locator("//div[@class='destination-grid']");
         this.noDestinationFoundText = page.locator('//div[@class="no-results"]//h3');
+        this.searchInput = page.locator('[data-test="dest-search-input"]');
+        this.categoryFilters = page.locator('//button[@class="category-btn "]');
+        this.categoryFiltersActive = page.locator('//button[@class="category-btn active"]');
+        this.cardBadge = page.locator('//div[@class="card-badge"]');
     }
 
     async navigate() {
@@ -24,9 +32,25 @@ export class DestinationPage {
         return await this.destinationGridContainer.all();
     }
 
+    async getCategoriesName(): Promise<Locator[]> {
+        return await this.categoryFilters.all();
+    }
+
+    async getCardBadges(): Promise<Locator[]> {
+        return await this.cardBadge.all();
+    }
+
+    async getActiveCategoryFilters(): Promise<string> {
+        return await this.categoryFiltersActive.first().textContent();
+    }
+
     async getNoDestinationFoundText(): Promise<string> {
         await expect(this.noDestinationFoundText).toBeVisible();
         return (await this.noDestinationFoundText.textContent()) ?? '';
+    }
+
+    async search(location: string) {
+        await this.searchInput.fill(location);
     }
 
     // async search(locationName: string) {
---
+++ test/tests/destination.spec.ts
@@ -0,0 +1,122 @@
+import { test, expect } from '../fixtures/fixtures';
+import { testData } from '../data/testData';
+
+test.describe('Destination page interactions @regression', () => {
+    test.beforeEach(async ({ destinationPage }) => {
+        await destinationPage.navigate();
+    });
+
+    /* Test with existed location names */
+    test('search existed location → result found @smoke', async ({ page, destinationPage }) => {
+        await destinationPage.search(testData.searchLocations.validLocationName);
+        await expect(page).toHaveURL(`${process.env.BASE_URL}/destinations`);
+
+        const count = await destinationPage.destinationGridCounter();
+        expect(count).toBeGreaterThanOrEqual(1);
+
+        const grids = await destinationPage.getAllDestinationGrids();
+
+        for (const grid of grids) {
+            await expect(grid).toBeVisible();
+        }
+    });
+
+    /* Test with existed country name */
+    test('search existed country name → result found @smoke', async ({ page, destinationPage }) => {
+        await destinationPage.search(testData.searchLocations.validCountryName);
+        await expect(page).toHaveURL(`${process.env.BASE_URL}/destinations`);
+
+        const count = await destinationPage.destinationGridCounter();
+        expect(count).toBeGreaterThanOrEqual(1);
+
+        const grids = await destinationPage.getAllDestinationGrids();
+
+        for (const grid of grids) {
+            await expect(grid).toBeVisible();
+        }
+    });
+
+    /* Test location that doesn't exists */
+    test('search location that does not exist → result not found found', async ({ page, destinationPage }) => {
+        await destinationPage.search(testData.searchLocations.invalidLocationName);
+        await expect(page).toHaveURL(`${process.env.BASE_URL}/destinations`);
+
+        const getText = await destinationPage.getNoDestinationFoundText();
+
+        expect(getText).toEqual('No destinations found');
+    });
+
+    /* Test location with blank filed */
+    test('search with blank field → field is required', async ({ destinationPage }) => {
+        await expect(destinationPage.searchInput).toHaveAttribute('required');
+    });
+
+    /* Test search location with trailing spaces */
+    test('search location with trailing spaces → must trim the spaces and result found', async ({
+        page,
+        destinationPage,
+    }) => {
+        await destinationPage.search(`   ${testData.searchLocations.validLocationName}   `);
+        await expect(page).toHaveURL(`${process.env.BASE_URL}/destinations`);
+
+        const count = await destinationPage.destinationGridCounter();
+        expect(count).toBeGreaterThanOrEqual(1);
+
+        const grids = await destinationPage.getAllDestinationGrids();
+
+        for (const grid of grids) {
+            await expect(grid).toBeVisible();
+        }
+    });
+
+    /* Test search location with different character cases */
+    test('search location with different character combinations → result found', async ({ page, destinationPage }) => {
+        const combinationList = [
+            testData.searchLocations.validLocationName.toUpperCase(),
+            testData.searchLocations.validLocationName.toLowerCase(),
+            testData.searchLocations.validLocationName.charAt(0).toUpperCase() +
+                testData.searchLocations.validLocationName.slice(1),
+        ];
+
+        for (const combination of combinationList) {
+            await destinationPage.search(combination);
+            await expect(page).toHaveURL(`${process.env.BASE_URL}/destinations`);
+
+            const count = await destinationPage.destinationGridCounter();
+            expect(count).toBeGreaterThanOrEqual(1);
+
+            const grids = await destinationPage.getAllDestinationGrids();
+
+            for (const grid of grids) {
+                await expect(grid).toBeVisible();
+            }
+
+            await destinationPage.navigate();
+        }
+    });
+
+    /* Test category filters with card badge */
+    test('select category → card badge equals to category type', async ({ destinationPage }) => {
+        const categories = await destinationPage.getCategoriesName();
+
+        for (const category of categories) {
+            await category.click();
+
+            const categoryType = await destinationPage.getActiveCategoryFilters();
+            const badges = await destinationPage.getCardBadges();
+
+            for (const badge of badges) {
+                await expect(badge).toBeVisible();
+
+                const cardBadgeText = await badge.textContent();
+
+                expect(categoryType).toEqual(cardBadgeText);
+            }
+        }
+    });
+    /* Test max price with 50 */
+    /* Test max price with 49 */
+    /* Test max price with 500 */
+    /* Test max price with 501 */
+    /* Test max price with 300 */
+});
